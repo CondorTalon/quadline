7ec36cb8759a3668fa83be033b9da8b71bf500d5
yunaian <naian.yu@mail.utoronto.ca>
2019-11-10 15:08:21 -0500

    update methods in QuadlineGrid.py and revamp docstring formats

--- QuadlineGrid.py
+++ QuadlineGrid.py
@@ -36,34 +36,49 @@ class QuadlineGrid:
     def get_grid(self) -> List[List[str]]:
         """
         Returns the Quadline grid
+
+        Parameters:
         :return: the game grid in nested list format
         """
         return self.grid
 
     def drop_token(self, col: int, token: str) -> None:
         """
-        Adds player token to next unoccupied row of the grid in the specified
-        col
-        :param col: int from 0-6
-        :param token: str 'P1' or 'P2'
+        Adds player token to first unoccupied row, starting from the bottom of the grid,
+        in the specified column.
+        
+        Parameters:
+        :int col: The column the token will drop into.
+        :str token: The string representation of a token. Can be 'P1' or 'P2'.
         :return: None
         """
-        y = 0
-        while y < 6 and self.has_token(y, col):
-            y += 1
-        self.grid[y-1][col] = token
+        if self.valid_location(col):
+            y = 0
+            while self.has_token(y, col):
+                y += 1
+            self.grid[y][col] = token
 
     def has_token(self, row: int, col: int) -> bool:
         """
-        Check row and col on grid to see if there is a token in that location
-        :param row: int from 0-5
-        :param col: int from 0-6
-        :return: bool depending on if there is a token in specified location
+        Check a specified row and column on grid to see if there exist a token in that
+        location.
+        
+        Parameters:
+        :int row: The row of the location being checked
+        :int col: The column of the location being checked
+        :return: True if there is a token on the location. False otherwise.
         """
-        return self.grid[row][col] == " "
+        return (not self.grid[row][col] == " ")
 
     def valid_location(self, col: int) -> bool:
         """
-        checks if the token can be dropped in player's choice of column
+        Checks if there is space in the column specified that new tokens can occupy.
+
+        Parameters:
+        :int col: The column being checked
+        :return: True if there is space for a token. False otherwise.
         """
-        return self.grid[0][col] == " "
+        for r in range(self.row):
+            if not self.has_token(r, col):
+                return True
+        return False
